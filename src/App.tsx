4f66794a60a889d20d23e33e2dbb0c672321b810
thanh.kiet <kiet13.itus@gmail.com>
2022-03-12 13:46:20 +0700

    add: spinning when fetching images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { imagesApis } from "apis";
 import ImageCardList from "components/ImageCardList";
 import SearchBar from "components/SearchBar";
+import Spin from "components/Spin";
 import { IImage } from "models";
 import { useCallback, useEffect, useState } from "react";
 
@@ -9,10 +10,13 @@ const { getImages } = imagesApis;
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [images, setImages] = useState<IImage[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   const fetchData = useCallback(async () => {
+    setIsFetching(true);
     const data = await getImages(searchTerm);
     setImages(data);
+    setIsFetching(false);
   }, [searchTerm]);
 
   useEffect(() => {
@@ -22,7 +26,11 @@ function App() {
   return (
     <div className="w-full py-8 px-10">
       <SearchBar submitTerm={setSearchTerm} />
-      <ImageCardList images={images} />
+      {isFetching ? (
+        <Spin className="mt-20 mx-auto w-10 h-10 fill-green-500" />
+      ) : (
+        <ImageCardList images={images} />
+      )}
     </div>
   );
 }
